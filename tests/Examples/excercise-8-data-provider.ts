29542ad2257bd2ba8bed787facaac558bdbb9c30
Fishgame1 <michal.wilkowski92@gmail.com>
2023-10-23 14:43:08 +0200

    add answers only

--- tests/Examples/excercise-8-data-provider.ts
+++
@@ -1,8 +0,0 @@
-
-export const getExcerciseEightTasks = () => {
-    return [
-    [{ taskName: "First one", isCompleted: false }, { taskName: "First two", isCompleted: false }],
-    [{ taskName: "Second one", isCompleted: false }],
-    [{ taskName: "Third one", isCompleted: false }, { taskName: "Third two", isCompleted: false }, { taskName: "Third three", isCompleted: false }]
-    ]
-};
